16a512c54dcd8cda60b5372e7d8e0b6b8bce517d
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-10-16 22:30:51 +0800

    update: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ npm run build
 git checkout master
 rm -rf main.js
 rm -rf index.html
-rm -rf common.?.js
-rm -rf common.?.css
+rm -rf common.*.js
+rm -rf common.*.css
 mv dist/* ./
 git add .
 git commit -m 'publish ++'
