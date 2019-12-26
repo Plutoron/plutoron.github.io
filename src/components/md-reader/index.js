04891b2809a628ccf59216641a94297a907c5293
yunlong.syl <yunlong.syl@dtwave-inc.com>
2019-12-26 19:14:35 +0800

    deploy

--- src/components/md-reader/index.js
+++ src/components/md-reader/index.js
@@ -1,11 +1,22 @@
-import {asyncComponent} from 'common/util'
+// import {asyncComponent} from 'common/util'
 
-export default asyncComponent(async () => {
-  try {
-    const module = await import('./md-reader')
-    return module.default
-  } catch (error) {
-    console.log(error)
-  }
-  return null
-})
+// export default asyncComponent(async () => {
+//   try {
+//     const module = await import('./md-reader')
+//     return module.default
+//   } catch (error) {
+//     console.log(error)
+//   }
+//   return null
+// })
+import { Suspense, lazy } from 'react'
+
+const Home = lazy(() => import('./md-reader'))
+
+const Main = () => (
+  <Suspense fallback='加载文章中'>
+    <Home />
+  </Suspense>
+)
+
+export default Main
