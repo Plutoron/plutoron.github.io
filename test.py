4d7411b1538c3a1ae7cfd3846b10ceed227edb7a
yunlong.syl <yunlong.syl@dtwave-inc.com>
2020-05-26 11:13:50 +0800

    deploy

--- test.py
+++ test.py
@@ -1,19 +1,47 @@
 class Solution:
-  def lengthOfLongestSubstring(self, s: str) -> int:
-    list = []
+  def longestPalindrome(self, s: str) -> str:
     max = 0
-    for index in range(s.__len__()):
-      cur_s = s[index]
-      try:
-        list = list[list.index(cur_s) + 1:]
-      except:
-        pass
-      list.append(cur_s)
-      max = len(list) if len(list) > max else max
+    mid = int(s.__len__() / 2)
 
-      print(list)
-    return max
+    for index, v in enumerate(s):
+      _range = index if index < mid else s.__len__() - mid
+      pre = ''
+      after = ''
+      length = 0
 
-lengthOfLongestSubstring = Solution().lengthOfLongestSubstring('asfgt')
+      # 中间对称
+      for cursor in range(index):
+        cursor = cursor + 1
+  
+        outRight = index - cursor < 0
+        outLeft = index + 1 + cursor > s.__len__()
 
-print(lengthOfLongestSubstring)
+        if (not outLeft) and (not outRight):
+          pre = s[(index - cursor):index]
+          after = s[(index + 1): (index + 1 + cursor)]
+
+          print(pre, after)
+
+      # 镜像对称
+      for cursor in range(index):
+        cursor = cursor + 1
+  
+        outRight = index - cursor < 0
+        outLeft = index + 2 + cursor > s.__len__()
+
+        if (not outLeft) and (not outRight):
+          pre = s[(index - cursor):(index + 1)]
+          after = s[(index + 1): (index + 2 + cursor)]
+
+          print(pre, after)
+
+
+
+
+    
+
+longestPalindrome = Solution().longestPalindrome('abcbc')
+
+print(longestPalindrome)
+
+print('sss'[1:2])
