4442be053a4283e081e87cdd90f5b80027763d5c
yunlong.syl <yunlong.syl@dtwave-inc.com>
2020-07-28 10:02:52 +0800

    更新

--- test.js
+++ test.js
@@ -15,3 +15,50 @@ function sum() {
 }
 
 console.log(sum(1)(2))
+
+
+class PopupBase {
+  show = false
+
+  constructor(priorty, group) {
+    // 初始化 并且添加到 队列
+    
+    if (group) {
+      window.popupStack.groups[group] = []
+    }
+  }
+
+  open() {
+    if (!window.popupStack.lock) {
+      this.show = true
+      window.popupStack.lock = true
+    } else {
+      this.judgeAction()
+    }
+  }
+
+  close() {
+    this.show = false
+
+    
+  }
+
+  judgeAction() {
+    
+    if (window.popupStack)
+    // 当前是否有显示中的popup 
+    window.popupTaskLock
+  }
+}
+
+window.popupStack = {
+  lock: false,
+  groups: {
+    'A': [1, 2, 3],
+  },
+  tasks: {
+    1: '',
+    2: '',
+    4: ''
+  }
+}
