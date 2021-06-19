1c4f034e1883ba9bbafd0f2711f8fd3b2b10bbec
yunlong <suyunlongsy@gmail.com>
2021-06-19 12:49:02 +0800

    no message

--- test/test26.js
+++ test/test26.js
@@ -1,7 +1,3 @@
-// 字符串仅由小写字母和 [] 组成，且字符串不会包含多余的空格。
-// 示例一: 'abc' --> {value: 'abc'}
-// 示例二：'[abc[bcd[def]]]' --> {value: 'abc', children: {value: 'bcd', children: {value: 'def'}}}
-
 const getIndex = (source, target) => {
   let min = 0
   let max = source.length - 1
@@ -21,5 +17,5 @@ const getIndex = (source, target) => {
   }
 }
 
-console.log(getValue([1,2,3,5,5,5,5,5,6,7], 5))
-console.log(getValue([3,3,4,6,7,8,9,10], 6))
+console.log(getIndex([1,2,3,5,5,5,5,5,6,7], 5))
+console.log(getIndex([3,3,4,6,7,8,9,10], 6))
